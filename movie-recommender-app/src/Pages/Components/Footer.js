77018cd8320a6dea420a85604af34c45c2b3a796
Pratham Golwala <prathamgolwala27@gmail.com>
2024-12-12 14:58:16 +0530

    First Commit

---
+++ movie-recommender-app/src/Pages/Components/Footer.js
@@ -0,0 +1,39 @@
+// Footer Component
+import "./styles/Footer.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer">
+                <div id="row1" className="row ">
+                    <a
+                        href="https://www.linkedin.com/in/pratham-golwala-6163ba203/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa fa-linkedin"></i>
+                    </a>
+                    <a
+                        href="https://github.com/pratham27-12"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa-brands fa-github"></i>
+                    </a>
+                    <a
+                        href="https://www.instagram.com/pratham_golwala_27/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa fa-instagram"></i>
+                    </a>
+                </div>
+                <div id="row1" className="row">
+                    © Developed By Pratham Golwala
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
